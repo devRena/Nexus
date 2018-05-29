32f44d8ab25ac3e5c6b2218aaebc9202941eea87
devRena <rkont@telcoserv.gr>
2018-05-29 16:59:50 +0300

    login changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,37 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ModalComponent } from './modal/modal.component';
 
 
+
+
+const appRoutes: Routes = [
+  {path:'home',component: HomeComponent},
+  {
+  path:'login',
+  component: LoginComponent
+},
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  { path: '**', component: LoginComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-	RouterModule.forRoot([
-	{path:'home',component: HomeComponent},
-	{path:'login',component: LoginComponent}
-	])
+	RouterModule.forRoot(
+	 appRoutes,
+      { enableTracing: true } 
+	)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+// app-modal.component.ts
+@Component({
+  selector: 'app-modal',
+  exportAs: 'modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent {
+  //@Output() modalClose : EventEmitter<any> = new EventEmitter<any>();
+
+  constructor(  ) {}
+  
+}
